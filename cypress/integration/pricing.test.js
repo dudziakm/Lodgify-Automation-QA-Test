5289c547883b82a40201b15f96cf1ccf425099bd
Michal Dudziak <michal.dudziak@iiit.pl>
2022-01-18 21:47:06 +0100

    switch to TS

--- cypress/integration/pricing.test.js
+++ cypress/integration/pricing.test.js
@@ -1,11 +1,33 @@
-beforeEach(()=>{
-  cy.openPricingPage();
+beforeEach(() => {
+  
 });
 
-context('Lodgify Pricing Page', () => {
-  it('Should have proper prices for all plans', () => {
-    cy.fixture('planPrices').then((planPrices) => {
-      cy.get('.price-item').contains('Starter').get('.plan-price')
-    })
+const priceCard = ".price-item";
+const pricePlan = ".plan-price";
+const priceScroll = '#scroll-prop-plan';
+
+const amountOfRentals = 50;
+
+const allPlansSet = ["Starter", "Professional", "Ultimate"];
+
+context(`Lodgify Pricing Page for ${amountOfRentals} Rentals` , () => {
+  before(() => {
+    cy.openPricingPage();
+    cy.get(priceScroll)
+      .invoke("val", amountOfRentals)
+      .trigger("change");
   });
-})
+
+  for (const plan of allPlansSet) {
+    it(`Should have proper prices for ${plan} Plan`, () => {
+      cy.fixture("planPricesFor50Rentals").then((planPrices) => {
+        cy.get(priceCard)
+          .contains(plan)
+          .parent()
+          .within(() => {
+            cy.get(pricePlan).should("contain.text", planPrices[plan]);
+          });
+      });
+    });
+  }
+});
