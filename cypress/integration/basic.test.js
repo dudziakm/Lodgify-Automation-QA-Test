05fd94f81e821eed0e95d8a46d21e20b6fcee204
Michal Dudziak <michal.dudziak@iiit.pl>
2022-01-18 19:46:45 +0100

    package lock and port change

--- cypress/integration/basic.test.js
+++ cypress/integration/basic.test.js
@@ -1,6 +1,6 @@
 context('Lodgify contact page', () => {
   it('Should have the right title', () => {
-    cy.visit('http://localhost:8080/Contact.html');
+    cy.visit('http://localhost:8084/contact.html');
     cy.title().should('include', 'Contact');
   });
 })
