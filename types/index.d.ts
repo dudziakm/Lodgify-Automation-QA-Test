950017f59445ff177ab98cc54d8bbeb5d7026ce2
Michal Dudziak <michal.dudziak@iiit.pl>
2022-01-19 16:33:37 +0100

    remove not neede elements

--- types/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace Cypress {
-  interface Chainable {
-    openHomePage(): void;
-    openContactPage(): void;
-    openPricingPage: (arg: null) => void;
-    checkPageTitle: (title: string) => void;
-  }
-}
